a76d6b015b4d9f5da53730f6694caf8e85b1fb10
Martine Gagne <martine.e.gagne@gmail.com>
2017-06-26 15:24:10 -0400

    created_how_old_are_you_program

--- exercise3.rb
+++ exercise3.rb
@@ -5,3 +5,8 @@ puts "Hello, #{name}"
 puts "Which house did the Sorting Hat place you in?"
 house = gets
 puts "Welcome to #{house}!"
+
+puts "How old are you?"
+age_in_years = gets.to_i
+birthyear = (2017 - age_in_years)
+puts "You were born in the year in #{birthyear}."
