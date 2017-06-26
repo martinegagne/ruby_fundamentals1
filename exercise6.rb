9957b6ee5539a1268c28a26359a5b492f87964ee
Martine Gagne <martine.e.gagne@gmail.com>
2017-06-26 19:25:53 -0400

    added_energy_level_program_feature

--- exercise6.rb
+++ exercise6.rb
@@ -1,17 +1,23 @@
 distance = 0
-pace = "test"
-while pace != "go home" do
+energy = 0
 
+while energy >= 0
   puts "Would you like to walk or run?"
   pace = gets.chomp
+# puts "Not enough fuel in the tank. Take a breather."
 
 if pace == "walk"
   distance += 1
+  energy += 1
   puts "Distance from home is #{distance} km."
 elsif pace == "run"
   distance += 5
+  energy -= 1
   puts "Distance from home is #{distance} km."
-else 
+elsif pace == "go home"
+  break
+else
   puts "Command does not exist."
+
 end
 end
