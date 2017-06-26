95003f62061b1b6d4b96aea31602c4595be989e6
Martine Gagne <martine.e.gagne@gmail.com>
2017-06-26 17:15:30 -0400

    completed_pizza_ordering_program

---
+++ exercise5.rb
@@ -0,0 +1,12 @@
+
+
+puts "How many pizzas do you want to order?"
+quantity = gets.chomp.to_i
+
+(quantity).times do |current_pizza|
+  puts "How many toppings for pizza #{current_pizza + 1}?"
+  toppings_quantity = gets
+
+  puts "You have ordered a pizza with #{toppings_quantity} toppings."
+
+end
